94d46a570aa8ee6848f1cc980881bd0703ff4dec
Magnus Schöllin <magnus.schollin@advania.se>
2024-10-18 15:24:38 +0200

    limited code to no external dependencies

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,9 +4,8 @@
 <link rel="stylesheet" type="text/css" href="/css/style.css" />
 <link rel="stylesheet" type="text/css" href="/css/buttons.css" />
 <meta name="csrf-token" content="{{ csrf_token() }}" />
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<link rel="stylesheet" href="/css/googlesymbols.css" />
+<script src="/js/jquery-3.7.1.min.js"></script>
 <script src="/js/xlsx.js"></script>
 <style>
     .material-symbols-outlined {
