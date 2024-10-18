5dde13f87b5360fc25f6d07a55978a54e8d92373
Magnus Schöllin <magnus.schollin@advania.se>
2024-10-18 16:08:27 +0200

    darkmode I believe it is fixed

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -11,18 +11,3 @@
         });    
     });
 </script>
-@if (session('darkmode') == 1)
-    <style>
-        html {
-            color: white !important;
-            background-color: #262626 !important;
-        }
-        .popupheader, header, footer, .ticket, iframe {
-            background-color: #6e6e6e !important;
-        }
-        input[type=text], input[type=number], input[type=date], input[type=datetime-local], select, textarea {
-            background-color: #6e6e6e !important;
-            color: white !important;
-        }
-    </style>
-@endif
